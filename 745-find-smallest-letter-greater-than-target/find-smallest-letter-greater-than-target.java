226b01ab76fba5981ccb9ac983393a802ed837d8
bandisreesatya <bandisreesatya@gmail.com>
2025-11-03 14:40:52 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (7.17%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -2,27 +2,21 @@ class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
         
         int n=letters.length;
-        boolean flag=false;
-        HashMap<Character,Integer> hash=new HashMap<>();
-        for(int i=0;i<n;i++)
+        int l=0;
+        int r=n-1;
+        char ans=letters[0];
+        while(l<=r)
         {
-            char ch=letters[i];
-            hash.put(ch,ch-'a');
+        int mid=(r+l)/2;
+        if(letters[mid]>target)
+        {
+           ans=letters[mid];
+           r=mid-1;
         }
-        int tn=target-'a';
-        int c=0;
-        for(int i=0;i<n;i++)
-          {
-            if(tn<hash.get(letters[i]))
-            {
-               flag=true;
-              c=hash.get(letters[i]);
-               break;
-            }
-          }
-          if(flag)
-           return (char)(c+'a');
         else
-        return letters[0];
+          l=mid+1;
+        }
+        return ans;
+
     }
 }
