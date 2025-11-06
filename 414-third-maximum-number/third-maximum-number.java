fd4236449f03c989da2f850147032fa680211812
bandisreesatya <bandisreesatya@gmail.com>
2025-11-06 13:03:00 +0530

    Time: 1 ms (97.39%) | Memory: 44.5 MB (34.05%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,13 +1,30 @@
-class Solution {
-    public int thirdMax(int[] nums) {
-        Set<Integer> set = new TreeSet<>();
-        int n=nums.length;
-        for(int i=0;i<n;i++)
-             set.add(nums[i]);
-        List<Integer> list = new ArrayList<>(set);
-        if(set.size()<3)
-            return list.get(set.size()-1);
-        else
-            return list.get(set.size()-3);
+class Solution{
+    public int thirdMax(int[] nums){
+        long max1=Long.MIN_VALUE;
+        long max2=Long.MIN_VALUE;
+        long max3=Long.MIN_VALUE;
+        for(int num:nums){
+            if(num==max1||num==max2||num==max3)
+            continue;
+            if(num>max1)
+               { 
+                max3=max2;
+                max2=max1;
+                max1=num;
+              }
+         else if(num>max2)
+           {
+                max3=max2;
+                max2=num;
+            }
+              else if(num>max3)
+               { 
+                  max3=num;
+            }
+        }
+        if(max3==Long.MIN_VALUE)
+        return (int)max1;
+        else 
+        return (int)max3;
     }
 }
