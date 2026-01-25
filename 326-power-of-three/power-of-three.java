c0960a4aa1441336e39fd36fe84e744b5a9b91f2
bandisreesatya <bandisreesatya@gmail.com>
2026-01-25 23:23:44 +0530

    Time: 8 ms (85.10%) | Memory: 46.3 MB (18.63%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -4,9 +4,8 @@ class Solution {
        if(n<=0) return false;
        while(n!=1)
        {
-        if(n%3!=0)
-          return false;
-          n/=3;
+        if(n%3!=0) return false;
+        n/=3;
        }
        return true;
 
