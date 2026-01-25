45834b51337f70285eebfdc5e17090b9865e691b
bandisreesatya <bandisreesatya@gmail.com>
2026-01-25 23:12:10 +0530

    Time: 9 ms (7.25%) | Memory: 46 MB (77.15%) - LeetSync

---
+++ 326-power-of-three/power-of-three.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfThree(int n)
+    {
+       if(n<=0) return false;
+       while(n!=1)
+       {
+        int rem=n%3;
+        if(rem!=0)
+          return false;
+          n/=3;
+       }
+       return true;
+
+
+    }
+}
