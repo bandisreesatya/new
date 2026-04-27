e6a259a1e71ad55fcbcac0727788a0120cb53885
bandisreesatya <bandisreesatya@gmail.com>
2026-04-27 12:05:59 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (95.95%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -12,8 +12,8 @@ class Solution {
             list.add("Buzz");
            }
            else{
-            //list.add(String.valueOf(i));
-            list.add(i+"");
+            list.add(String.valueOf(i));
+            //list.add(i+"");
            }
         }
         return list;
