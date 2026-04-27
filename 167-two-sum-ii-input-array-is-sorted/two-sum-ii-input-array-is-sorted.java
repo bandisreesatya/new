2859b0b6cd2a130b402490a2212ce4e83ad11558
bandisreesatya <bandisreesatya@gmail.com>
2026-04-27 15:11:05 +0530

    Time: 2 ms (96.46%) | Memory: 48.3 MB (79.76%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -3,13 +3,10 @@ class Solution {
         int n=numbers.length;
         int j=n-1;
         int i=0;
-        int[] arr=new int[2];
         while(i<j){
             int sum=numbers[i]+numbers[j];
             if(sum==target){
-                arr[0]=i+1;
-                arr[1]=j+1;
-                return arr;
+                return new int[]{i+1,j+1};
             }
             else if (sum>target){
                 j--;
@@ -18,6 +15,6 @@ class Solution {
                 i++;
             }
         }
-        return arr;
+       return new int[2];
     }
 }
