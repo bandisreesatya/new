92c88067fd3d39d07cdd844f0b8eea56b8876e94
bandisreesatya <bandisreesatya@gmail.com>
2026-04-29 14:58:29 +0530

    Time: 6 ms (84.59%) | Memory: 46.3 MB (99.36%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -5,8 +5,9 @@ class Solution {
             freq[c-'a']++;
         }
         for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(freq[ch-'a']==1) return i;
+            if(freq[s.charAt(i)-'a']==1) {
+                return i;
+            }
         }
         return -1;
     }
